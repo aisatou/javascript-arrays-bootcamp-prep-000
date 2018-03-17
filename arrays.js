700f606e9b118cd8927519428dc4b3d1879bc32d
Aisatou Diallo <diallo.aisatou@gmail.com>
2018-03-17 05:46:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,7 +30,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 
 function removeElementFromBeginningOfArray(array) {
   array = array.slice(0, array.length - 1);
-  return array;
+  return 
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
